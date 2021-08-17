4ac9e5f7aefc7f0208ab9878a6cdda226b332927
prasesh729 <praseshdangol@gmail.com>
2021-08-17 22:50:58 +0545

    first commit

---
+++ src/com/login/loginpage.java
@@ -0,0 +1,9 @@
+package com.login;
+
+public class loginpage {
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+
+
+    }
+}
---
+++ src/main/smsclass/SchoolMainClass.java
@@ -0,0 +1,7 @@
+package main.smsclass;
+
+public class SchoolMainClass {
+    public static void main(String[] args) {
+
+    }
+}
